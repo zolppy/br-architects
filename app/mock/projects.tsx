4a3958fc80228825e2f7797b9860fa3b0bf265de
Gabriel Cavalcante <gabriel.cavalcant.pb@compasso.com.br>
2024-12-02 00:16:01 -0300

    fix: nome de propriedade no mock de projetos

--- app/mock/projects.tsx
+++ app/mock/projects.tsx
@@ -7,7 +7,7 @@ import house4 from "@/public/houses/house4.jpg";
 const projects: IProject[] = [
     {
         id: 1,
-        title: "Summer House",
+        projectTitle: "Summer House",
         img: {
             src: house1,
             alt: "A house with light gray siding, with two rectangular windows on the upper floor and one on the ground floor. The house is located on a lot with tall vegetation, and a wooden deck with handrails leads to the front door. The scene is illuminated by soft light, suggesting late afternoon or early evening.",
@@ -16,7 +16,7 @@ const projects: IProject[] = [
     },
     {
         id: 2,
-        title: "Brick House",
+        projectTitle: "Brick House",
         img: {
             src: house2,
             alt: "A row of brick houses with black roofs, two chimneys and rectangular windows. The houses have a symmetrical design and are separated by a small garden. The street in front of the houses is paved and there is a lamppost next to it.",
@@ -25,7 +25,7 @@ const projects: IProject[] = [
     },
     {
         id: 3,
-        title: "Renovated",
+        projectTitle: "Renovated",
         img: {
             src: house3,
             alt: "Front view of a single-family home with a modern design and straight lines. The house is situated on a flat lot with tall grass and a night sky with thick clouds. Lights on in the windows create a cozy atmosphere.",
@@ -34,7 +34,7 @@ const projects: IProject[] = [
     },
     {
         id: 4,
-        title: "Barn House",
+        projectTitle: "Barn House",
         img: {
             src: house4,
             alt: "A contemporary house with a dark wood-clad facade and a pitched roof. The roof features several glass windows, providing natural light to the interior. In the background, a clear blue sky with a few clouds serves as a backdrop for the building.",
@@ -43,7 +43,7 @@ const projects: IProject[] = [
     },
     {
         id: 5,
-        title: "Summer House",
+        projectTitle: "Summer House",
         img: {
             src: house2,
 
@@ -53,7 +53,7 @@ const projects: IProject[] = [
     },
     {
         id: 6,
-        title: "Brick House",
+        projectTitle: "Brick House",
         img: {
             src: house1,
             alt: "A house with light gray siding, with two rectangular windows on the upper floor and one on the ground floor. The house is located on a lot with tall vegetation, and a wooden deck with handrails leads to the front door. The scene is illuminated by soft light, suggesting late afternoon or early evening.",
@@ -62,7 +62,7 @@ const projects: IProject[] = [
     },
     {
         id: 7,
-        title: "Renovated",
+        projectTitle: "Renovated",
         img: {
             src: house4,
             alt: "A contemporary house with a dark wood-clad facade and a pitched roof. The roof features several glass windows, providing natural light to the interior. In the background, a clear blue sky with a few clouds serves as a backdrop for the building.",
@@ -71,7 +71,7 @@ const projects: IProject[] = [
     },
     {
         id: 8,
-        title: "Barn House",
+        projectTitle: "Barn House",
         img: {
             src: house3,
             alt: "Front view of a single-family home with a modern design and straight lines. The house is situated on a flat lot with tall grass and a night sky with thick clouds. Lights on in the windows create a cozy atmosphere.",
