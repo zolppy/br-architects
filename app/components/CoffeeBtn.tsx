b3d1dd7656a4a0631ae790f18967f0407745f9b4
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-04-19 12:41:05 -0300

    fix: coffee btn

--- app/components/CoffeeBtn.tsx
+++ app/components/CoffeeBtn.tsx
@@ -5,7 +5,7 @@ const CoffeeBtn = () => {
         <a
             target="_blank"
             href="https://ko-fi.com/zolppy"
-            className="fixed -translate-y-1/2 -translate-x-1/2 bg-[#f44336] text-4xl p-3 rounded-full right-0 bottom-0 active:bg-black lg:hover:cursor-pointer lg:hover:bg-black text-white"
+            className="fixed -translate-y-1/2 -translate-x-1/2 bg-[#f44336] text-4xl p-3 rounded-full right-0 bottom-0 active:bg-black lg:hover:cursor-pointer lg:hover:bg-black text-white z-50"
         >
             <BsCupHotFill />
         </a>
